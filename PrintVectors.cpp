ce0918cc56ad14c0ad884c3a898b7f96cffdd040
James <Jaburro@live.com>
2021-07-07 01:44:29 -0400

    Printing Vector's
    Example of printing vector's line by line in C++.

---
+++ PrintVectors.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using std::cout;
+using std::vector;
+
+// TODO: Add PrintBoard function here.
+void PrintBoard(const vector<vector<int>> board)
+{
+    for (int i = 0; i < board.size(); i++)
+    {
+        for (int j = 0; j < board[i].size(); j++)
+        {
+            cout << board[i][j];
+        };
+        cout << "\n";
+    };
+}
+
+int main()
+{
+    vector<vector<int>> board{{0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 1, 0, 0, 0, 0},
+                              {0, 0, 0, 0, 1, 0}};
+    // TODO: Call PrintBoard function here.
+    PrintBoard(board);
+}
